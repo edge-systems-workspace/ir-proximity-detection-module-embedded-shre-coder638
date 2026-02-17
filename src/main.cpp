5b9009259ef076fc45646d286387d3b845a43e9d
shre-coder638 <shankarshreyash79@gmail.com>
2026-02-17 21:21:27 +0530

    Configure IR pin as INPUT

--- src/main.cpp
+++ src/main.cpp
@@ -19,12 +19,7 @@ void setup() {
 
     Serial.begin(9600);
 
-    // TODO 4:
-    // Configure IR pin as INPUT
     pinMode(IR_PIN, INPUT);
-
-    // TODO 5:
-    // Print system initialization message
     Serial.println("IR Obstacle Detection System Initialized");
 }
 
