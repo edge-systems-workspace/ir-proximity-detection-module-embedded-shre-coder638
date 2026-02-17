0ad68e9cf573c861cfd7fd5488d36ac2e5c1c3fd
shre-coder638 <shankarshreyash79@gmail.com>
2026-02-17 20:58:31 +0530

    Initialize Serial communication at 9600 baud rate

--- src/main.cpp
+++ src/main.cpp
@@ -16,14 +16,9 @@ int fsrValue = 0;
 
 void setup() {
 
-    // TODO 3:
-    // Initialize Serial communication (9600 baud rate)
+    Serial.begin(9600);
 
-    // TODO 4:
-    // Configure IR pin as INPUT
-
-    // TODO 5:
-    // Print system initialization message
+    Serial.println("=== FSR Force Measurement System Initialized ===");
 }
 
 void loop() {
