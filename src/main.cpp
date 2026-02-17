9f46a803e7537faa6fb0908ac5733f7d6bf7948a
shre-coder638 <shankarshreyash79@gmail.com>
2026-02-17 20:57:11 +0530

    Initial FSR analog pin

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,9 @@
  * and detects obstacle presence.
  */
 
- // TODO 1:
- // Define IR sensor digital pin (Use pin 2)
+#define FSR_PIN A0
 
- // TODO 2:
- // Create variable to store sensor state
+int fsrValue = 0;
 
 void setup() {
 
